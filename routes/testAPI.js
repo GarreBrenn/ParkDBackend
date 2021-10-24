be451f99d1c945eba04705c85c0856be8600148a
Richard <mccartyra@g.cofc.edu>
2021-10-24 14:43:03 -0400

    Enabled filtering

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,33 +3,40 @@ var router = express.Router();
 var main = require("../testBlockchain");
 router.post('/query',  async (req, res, next) => {
     let output = await main.query();
-    if(req.body.startDate != null) {
-        let startDate = new Date(req.body.startDate)
-        output.filter((d) => {
-            if(startDate < d.ReservationTimeIn) {
-                return false;
-            }
-            return true;
-        })
+    console.log(req.body)
+    try {
+        let input = req.body;
+        if(input.startDate != null) {
+            let startDate = new Date(input.startDate)
+            output.filter((d) => {
+                if(startDate < d.ReservationTimeIn) {
+                    return false;
+                }
+                return true;
+            })
+        }
+        if(input.endDate != null) {
+            let endDate = new Date(input.endDate)
+            output.filter((d) => {
+                if(endDate > d.ReservationTimeOut) {
+                    return false;
+                }
+                return true;
+            })
+        }
+        if(input.price != null) {
+            output.filter((d) => {
+                if(input.price < d.Price) {
+                    return false;
+                }
+                return true;
+            })
+        }
+        res.send(output);
     }
-    if(req.body.endDate != null) {
-        let endDate = new Date(req.body.endDate)
-        output.filter((d) => {
-            if(endDate > d.ReservationTimeOut) {
-                return false;
-            }
-            return true;
-        })
+    catch(e) {
+        res.send(e)
     }
-    if(req.body.price != null) {
-        output.filter((d) => {
-            if(req.body.price < d.Price) {
-                return false;
-            }
-            return true;
-        })
-    }
-    res.send(output);
 });
 router.post('/buy', async (req, res, next) => {
     let output = await main.purchaseSpotAsset(req.body.id, req.body.timeIn, req.body.timeOut);
