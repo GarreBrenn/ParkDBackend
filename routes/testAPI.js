2bf1f58bf2e238c9dff6c212a5a99cad4696668c
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-11-15 20:24:41 -0500

    Testing allAsset logging

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,34 +3,36 @@ var router = express.Router();
 var main = require("../testBlockchain");
 router.post('/query',  async (req, res, next) => {
     let output = await main.query();
-    output = JSON.parse(output)
-    try {
-        let input = req.body;
-        if(input.startDate != null) {
-            let startDate = new Date(input.startDate)
-            output.filter((d) => {
-                let flag = true;
-                for(let entries in d.Record.Reservations) {
-                    if(!(startDate > entries.resTimeIn && input.endDate > entries.resTimeOut) || !(startDate < entries.resTimeIn && input.endDate < entries.resTimeOut)) {
-                        flag = false;
+    parseAssets(output).then((output, bad) => {
+        try {
+            let input = req.body;
+            if(input.startDate != null) {
+                let startDate = new Date(input.startDate)
+                output.filter((d) => {
+                    let flag = true;
+                    for(let entries in d.Record.Reservations) {
+                        if(!(startDate > entries.resTimeIn && input.endDate > entries.resTimeOut) || !(startDate < entries.resTimeIn && input.endDate < entries.resTimeOut)) {
+                            flag = false;
+                        }
                     }
-                }
-                return flag;
-            })
+                    return flag;
+                })
+            }
+            if(input.price != null) {
+                output.filter((d) => {
+                    if(input.price < d.Price) {
+                        return false;
+                    }
+                    return true;
+                })
+            }
+            res.send(output);
         }
-        if(input.price != null) {
-            output.filter((d) => {
-                if(input.price < d.Price) {
-                    return false;
-                }
-                return true;
-            })
+        catch(e) {
+            res.send(e)
         }
-        res.send(output);
-    }
-    catch(e) {
-        res.send(e)
-    }
+    })
+
 });
 router.post('/buy', async (req, res, next) => {
     let output = await main.purchaseSpotAsset(req.body.id, req.body.timeIn, req.body.timeOut);
@@ -47,21 +49,31 @@ router.post('sell', async (req, res, next) => {
 
 router.post('/reserve', async (req, res, next) => {
     let allAssets = await main.query();
-    allAssets = await JSON.parse(allAssets)
-    console.log(allAssets);
-    for(let asset in allAssets) {
-        console.log(asset)
-        if(asset.Record.id === req.body.id) {
-            let curReservations = asset.Record.Reservations;
-            curReservations.push({
-                resTimeIn: req.body.timeIn,
-                resTimeOut: req.body.timeOut,
-                guestId: req.body.guestId,
-            })
-            await main.appendCheckin(req.body.id, curReservations);
-            break;
+    parseAssets(allAssets).then((good, bad) => {
+        for(let asset in good) {
+            console.log(asset)
+            if(asset.Record.id === req.body.id) {
+                let curReservations = asset.Record.Reservations;
+                curReservations.push({
+                    resTimeIn: req.body.timeIn,
+                    resTimeOut: req.body.timeOut,
+                    guestId: req.body.guestId,
+                })
+                await main.appendCheckin(req.body.id, curReservations);
+                break;
+            }
         }
-    }
+    })
 })
-
+function parseAssets(asset) {
+    return new Promise((resolve, reject) => {
+        let output = JSON.parse(asset);
+        if(output) {
+            resolve(output);
+        }
+        else {
+            reject(output);
+        }
+    })
+}
 module.exports = router;
