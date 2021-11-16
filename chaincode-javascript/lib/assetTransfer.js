42cf5048ee68e491bc0e3bbb0761de14869e6c10
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-11-16 15:21:40 -0500

    Fixing Reservation Chaincode

--- chaincode-javascript/lib/assetTransfer.js
+++ chaincode-javascript/lib/assetTransfer.js
@@ -333,11 +333,8 @@ class AssetTransfer extends Contract {
     async ReserveAsset(ctx, id, reservation) {
         const assetString = await this.ReadAsset(ctx, id);
         const asset = JSON.parse(assetString);
-        let output = ""
-        for(let i = 0; i < reservation.length; i++) {
-        	output += JSON.stringify(reservation[i])
-        }
-        asset.Reservations = output;
+        reservation = reservation.join();
+        asset.Reservations = reservation;
         return ctx.stub.putState(id, Buffer.from(JSON.stringify(asset)));
     }
 }
